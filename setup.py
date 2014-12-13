b95e82734f67850216613fd7c0fb69541ee41ebb
Tobias Lohner <tobias@lohner-net.de>
2014-12-13 13:28:13 +0100

    Adapt required package versions to debian jessie's python packages.
    This will enable us to use the debian jessie packages to benefit
    from security updates to those packages.

--- setup.py
+++ setup.py
@@ -15,35 +15,38 @@ setup(
     url=about['__uri__'],
     packages=find_packages(),
     install_requires=[
-        'flask==0.10.1',
-        'werkzeug==0.9.6',
-        'Flask-Babel==0.9',
+         # Packages with a package name behind are available in debian jessie.
+         # Their version should not be changed unless the debian package
+         # version has changed.
+        'flask==0.10.1', # python-flask
+        'werkzeug==0.9.6', # python-werkzeug
+        'Flask-Babel==0.9', # python-flask-babel
         'Flask-Assets==0.8',
-        'Flask-Login==0.2.9',
+        'Flask-Login==0.2.6', # python-flask-login
         'Flask-Cache==0.12',
-        'Flask-Migrate==1.2.0',
-        'Flask-Script==0.6.7',
-        'Flask-SQLAlchemy==1.0',
-        'Flask-WTF==0.9.5',
-        'sqlalchemy==0.8.2',
-        'alembic==0.6.3',
-        'psycopg2==2.5.2',
+        'Flask-Migrate==1.2.0', # python-flask-migrate
+        'Flask-Script==0.6.7', # python-flask-script
+        'Flask-SQLAlchemy==1.0', # python-flask-sqlalchemy
+        'Flask-WTF==0.10.2', # python-flaskext.wtf
+        'sqlalchemy==0.9.8', # python-sqlalchemy
+        'alembic==0.6.5', # python-alembic
+        'psycopg2==2.5.4', # python-psycopg2
         'GeoAlchemy2==0.2.3',
-        'Shapely==1.3.0',
-        'crcmod==1.7',
-        'Markdown==2.4',
-        'pytz',
-        'webassets==0.8',
-        'cssmin==0.1.4',
+        'Shapely==1.4.3', # python-shapely
+        'crcmod==1.7', # python-crcmod
+        'Markdown==2.5.1', # python-markdown
+        'pytz', # python-tz
+        'webassets==0.10.1', # python-webassets
+        'cssmin==0.2', # cssmin
         'closure==20140110',
-        'WebHelpers==1.3',
-        'celery[redis]>=3.1,<3.2',
+        'WebHelpers==1.3', # python-webhelpers
+        'celery[redis]==3.1.13', # python-celery, python-redis
         'xcsoar==0.4',
-        'Pygments==1.6',
+        'Pygments==2.0.1', # python-pygments
         'aerofiles==0.1.1',
-        'enum34==1.0',
-        'pyproj==1.9.3',
-        'gevent==1.0.1',
+        'enum34==1.0.3', # python-enum34
+        'pyproj==1.8.9', # python-pyproj
+        'gevent==1.0.1', # python-gevent
     ],
     include_package_data=True,
     package_data={
